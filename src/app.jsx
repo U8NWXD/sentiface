505da9737326ba80280057fe1553d32281fedc60
cs <cs.temporary@icloud.com>
2019-02-17 04:29:52 +0000

    Add file selection and display of selected files

--- src/app.jsx
+++ src/app.jsx
@@ -17,17 +17,66 @@ class RunPython extends React.Component {
     }
 
     render() {
-        return (<div>{this.state.output}</div>)
+        return (<div>{this.state.output}</div>);
     }
 }
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            paths: null,
+        }
+    }
+
+    selectFiles() {
+        const { dialog } = require('electron').remote;
+        let selected_files = dialog.showOpenDialog({
+            properties: ["openFile"],
+            title: "Select Video",
+            defaultPath: "~",
+            buttonLabel: "Analyze",
+            filters: [
+                { name: "Movies", extensions: ["avi", "mp4"]},
+                { name: "TESTING", extensions: ["*"]},
+            ],
+        });
+        this.setState({
+            paths: selected_files,
+        });
+        return selected_files;
+    }
+
+    analyze() {
+        const files = this.state.paths;
+        if (!files) {
+            alert("You must select a file to analyze.");
+            return;
+        }
+        alert("TODO: Analyze these files: " + files.toString("utf8"));
+    }
+
     render() {
+        let files = "No Files Selected";
+        
+        if (this.state.paths) { 
+            files = this.state.paths.map((path) => {
+                return(
+                    <li key={path}>{path}</li>
+                );
+            });
+        }
+        
         return (
             <div>
                 <h2>Welcome to React!</h2>
+                
                 <h3>Output from Python:</h3>
                 <RunPython />
+                <button onClick={() => this.analyze()}>Analyze Files</button> 
+                <h3>Selected Files:</h3>
+                <button onClick={() => this.selectFiles()}>Select Files</button>
+                <div>{files}</div>
             </div>
         );
     }
