5306170c0365dd4beea264401d4a9ffcf8e0ae44
cs <cs.temporary@icloud.com>
2019-02-17 03:31:55 +0000

    Run and capture output from python script

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
+class RunPython extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            output: "Awaiting output from Python ... ",
+        };
+        
+        // SOURCE: https://www.techiediaries.com/python-electron-tutorial/
+        var python = require('child_process').spawn('python3', ['./src/dummy.py']);
+        python.stdout.on('data', (data) =>  {
+            this.setState({
+                output: data.toString('utf8'),
+            });
+        });
+    }
+
+    render() {
+        return (<div>{this.state.output}</div>)
+    }
+}
+
 export default class App extends React.Component {
-  render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
-  }
+    render() {
+        return (
+            <div>
+                <h2>Welcome to React!</h2>
+                <h3>Output from Python:</h3>
+                <RunPython />
+            </div>
+        );
+    }
 }
---
+++ src/dummy.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import sys
+
+print("Hello, world from python!")
+sys.stdout.flush()
