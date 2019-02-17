39a23b4116b8d6ff9bb510951e351624bee765b4
cs <cs.temporary@icloud.com>
2019-02-17 13:00:57 +0000

    Add graph for data visualization

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,7 @@ import Modal from 'react-bootstrap/Modal';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Badge from 'react-bootstrap/Badge';
 import * as d3 from 'd3';
+import {XYPlot, VerticalBarSeries, XAxis, YAxis} from 'react-vis';
 
 /*
  * props:
@@ -89,6 +90,70 @@ class RunPython extends React.Component {
             />
             </ButtonToolbar>
         );
+    }
+}
+
+function BarPlot(props) {
+    console.log(props);
+    const data = props.data
+    const height = props.height ? props.height : 300;
+    const width = props.width ? props.width : 300;
+    // SOURCE: https://stackoverflow.com/a/10050831
+    const x_labels = props.x_labels ? props.x_labels : [...Array(data.length).keys()];
+    const margin = {left: 40, right: 10, top: 10, bottom: 100}
+    return(
+        <div>
+            <XYPlot height={height} width={width} margin={margin}>
+                <VerticalBarSeries data={data} />
+                <XAxis tickFormat={i => x_labels[i]} tickLabelAngle={-90}/>
+            </XYPlot>
+        </div>
+    );
+}
+
+class VisualizeData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+            path: null,
+        };
+        this.state.path = "../sotu.csv";
+    }
+
+    loadData(path) {
+        if (!path) {
+            alert("No data file selected.");
+        }
+        d3.csv(path).then((loaded) => this.setState({data: loaded}));
+    }
+
+    render() {
+        if (this.state.data) {
+            let emotions = ["anger", "contempt", "disgust", "fear", "happiness",
+                "sadness", "smile", "surprise"];
+            let toDisplay = [];
+            let sumCallback = (acc, cur) => (acc + cur);
+
+            for (let i = 0; i < emotions.length; i ++) {
+                toDisplay[i] = {
+                    x: i,
+                    // SOURCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+                    y: this.state.data.map(elem => 
+                        100 * parseFloat(elem[emotions[i]]))
+                        .reduce(sumCallback, 0.0),
+                }
+            }
+            console.log(toDisplay);
+            return (
+                <BarPlot data={toDisplay} height={400} width={400} x_labels={emotions}/>
+            );
+        } else {
+            return ( 
+                <Button variant="primary" onClick={() => 
+                    this.loadData(this.state.path)}>Load Data</Button>
+            );
+        }
     }
 }
 
